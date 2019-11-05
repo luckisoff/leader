367fd0abf58701e9e1c42dfde48c8e37d6b2b44b
SRBN Media Nepal <srbnmedianepal@gmail.com>
2019-11-05 11:47:04 +0545

    new update

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -135,30 +135,32 @@
 
    <?php echo Setting::get('body_scripts'); ?>
    <script>
-    //    var ctx = document.getElementById('myChart');
-    //    var myBarChart = new Chart(ctx, {
-    //         type: 'bar',
-    //         data: {
-    //             labels:[],
-    //             datasets: [{
-    //                     barPercentage: 0.5,
-    //                     barThickness: 6,
-    //                     maxBarThickness: 8,
-    //                     minBarLength: 2,
-    //                     label: 'Rs Total (Top 10)',
-    //                     data: [],
-    //                     backgroundColor : [
-    //                         @php($colors=['pink','violet','red','green', 'blue', 'purple','magenta','orange','red'])
-    //                         @foreach(//$leaderboards as $leaderboard)
-    //                             "",
-    //                         @endforeach
-    //                     ],
-    //                     borderColor : [
-    //                         
-    //                     ],
-    //                 }]
-    //             },
-    //     });
+       var ctx = document.getElementById('myChart');
+       var myBarChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels:[@foreach($leaderboards as $leaderboard)"{{$leaderboard->user['name']}}",@endforeach],
+                datasets: [{
+                        barPercentage: 0.5,
+                        barThickness: 6,
+                        maxBarThickness: 8,
+                        minBarLength: 2,
+                        label: 'Rs Total (Top 10)',
+                        data: [@foreach($leaderboards as $leaderboard){{$leaderboard->point/10}},@endforeach],
+                        backgroundColor : [
+                            @php($colors=['pink','violet','red','green', 'blue', 'purple','magenta','orange','red'])
+                            @foreach($leaderboards as $leaderboard)
+                                "{{$colors[array_rand($colors)] }}",
+                            @endforeach
+                        ],
+                        borderColor : [
+                            @foreach($leaderboards as $leaderboard)
+                                "#111",
+                            @endforeach
+                        ],
+                    }]
+                },
+        });
     </script>
 </body>
 
