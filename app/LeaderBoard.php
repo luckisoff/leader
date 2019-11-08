7481680f626569a30a7ee9744a94b6b274ea66d9
SRBN Media Nepal <srbnmedianepal@gmail.com>
2019-11-08 12:28:12 +0545

    new update

--- app/LeaderBoard.php
+++ app/LeaderBoard.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 class LeaderBoard extends Model
 {
-    protected $fillable=['user_id','point','level'];
+    protected $fillable=['user_id','point','level','payment_claim'];
     
     public function user(){
         return $this->belongsTo(User::class);
