a41bc3363e3b9d3acd76ae263817059e21fdb173
SRBN Media Nepal <srbnmedianepal@gmail.com>
2019-12-02 15:53:42 +0545

    khalti integration

--- app/Http/Controllers/Api/KhaltiPaymentController.php
+++ app/Http/Controllers/Api/KhaltiPaymentController.php
@@ -11,28 +11,29 @@ class KhaltiPaymentController extends Controller
 {
     public function initiate(Request $request )
     {
-        $url='https://khalti.com/api/payment/initiate/';
+        return $request;
+        // $url='https://khalti.com/api/payment/initiate/';
 
        
-        $data=[
-            'public_key'=>config('services.khalti.client_id'),
-            'amount'=>1000*100,
-            'product_identity'=>'Leader Audition Registration',
-            'product_name'=>'Leader Audition Registration Charge',
-            'mobile'=>$request->mobile
+        // $data=[
+        //     'public_key'=>config('services.khalti.client_id'),
+        //     'amount'=>1000*100,
+        //     'product_identity'=>'Leader Audition Registration',
+        //     'product_name'=>'Leader Audition Registration Charge',
+        //     'mobile'=>$request->mobile
 
-        ];
-        # Make the call using API.
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-        // Response
-        $response=curl_exec($curl);
-        $status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        curl_close($curl);
-        return $response;
+        // ];
+        // # Make the call using API.
+        // $curl = curl_init();
+        // curl_setopt($curl, CURLOPT_URL, $url);
+        // curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        // curl_setopt($curl, CURLOPT_POST, true);
+        // curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        // // Response
+        // $response=curl_exec($curl);
+        // $status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        // curl_close($curl);
+        // return $response;
     }
 
     public function confirmation(Request $request)
