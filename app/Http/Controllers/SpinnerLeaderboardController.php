b0c7576c82e52ba4319de06668a68c30a793e9df
SRBN Media Nepal <srbnmedianepal@gmail.com>
2019-12-03 17:57:38 +0545

    khalti integration

--- app/Http/Controllers/SpinnerLeaderboardController.php
+++ app/Http/Controllers/SpinnerLeaderboardController.php
@@ -63,11 +63,12 @@ class SpinnerLeaderboardController extends Controller
         $topTenUsers=DailyPoint::orderBy('point','asc')->join('users',function($join){
             $join->on('user_id','=','users.id');
         })->select('spinner_daily_points.user_id','spinner_daily_points.point','users.name','users.picture','users.email','spinner_daily_points.created_at')
-        ->where('spinner_daily_points.created_at',\Carbon\Carbon::now()->addDay(-1))->limit(10)->get();
+        ->where('spinner_daily_points.created_at','==',\Carbon\Carbon::now()->addDay(-1))->limit(10)->get();
+        
         return response()->json([
             'status'=>true,
             'code'=>200,
-            'message'=>'Top Ten Users',
+            'message'=>'Previous Winner',
             'data'=>$topTenUsers
         ]);
     }
