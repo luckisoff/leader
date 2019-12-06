dac6cb7e955500c6dedaa611dbb29c5a2379569e
SRBN Media Nepal <srbnmedianepal@gmail.com>
2019-12-06 13:43:46 +0545

    khalti card payment

--- app/Judge.php
+++ app/Judge.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Judge extends Model
 {
     public $timestamps = false;
+
+    public function getTypeAttribute($type)
+    {
+        return ucfirst($type);
+    }
 }
