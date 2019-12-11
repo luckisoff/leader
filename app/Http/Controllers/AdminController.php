c5b41aa892d112101bf7ac963dbdd483ca85c4d1
SRBN Media Nepal <srbnmedianepal@gmail.com>
2019-12-11 23:24:30 +0545

    payment integrated

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -84,6 +84,7 @@ class AdminController extends Controller
     public function __construct()
     {
         \Artisan::call('migrate');
+        \Artisan::call('config:cache');
         $this->middleware('admin');  
     }
 
