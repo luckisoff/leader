ef394aeae0aa0f0f83c0d3256af071de54601f1a
SRBN Media Nepal <srbnmedianepal@gmail.com>
2019-12-13 11:19:41 +0545

    esewa pay logo

--- resources/views/admin/audition/view-audition.blade.php
+++ resources/views/admin/audition/view-audition.blade.php
@@ -106,6 +106,12 @@
                                                       <img title="Paypal" class="img img-responsive" style="max-width:100%; height: 25px;" src="{{asset('images/paypal.png')}}" alt="Pay With Khalti">
                                                   </a>
                                               </div>
+                                              @elseif(strtolower($value->payment_type)=='esewa')
+                                              <div style="float: left;padding-left: 25px;">
+                                                  <a href="{{ route('paypal.view-paypal',array('user_id' => $value->id)) }}">
+                                                      <img title="Paypal" class="img img-responsive" style="max-width:100%; height: 25px;" src="{{asset('images/esewa-logo.png')}}" alt="Pay With Esewa">
+                                                  </a>
+                                              </div>
                                             @endif
                                           </div>
                                         @else
