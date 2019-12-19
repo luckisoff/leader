348b9b60419fea321dcbf07760f6f8c2fa80b070
SRBN Media Nepal <srbnmedianepal@gmail.com>
2019-12-19 16:46:29 +0545

    update

--- app/Http/Controllers/Payment/WebPaymentController.php
+++ app/Http/Controllers/Payment/WebPaymentController.php
@@ -275,7 +275,7 @@ class WebPaymentController extends Controller
                 ]);
             }
             return response()->json([
-                "response_id"=>$request_id,
+                "request_id"=>$request_id,
                 "response_code"=>0,
                 "response_message"=>'success',
                 "amount"=>1000,
