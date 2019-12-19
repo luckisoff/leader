a5c96ce70aa6abf7cf1c718868d01110baa4e623
SRBN Media Nepal <srbnmedianepal@gmail.com>
2019-12-19 13:44:28 +0545

    esewa token

--- routes/api.php
+++ routes/api.php
@@ -123,5 +123,7 @@ Route::get('/audition/payment/esewa-pay/{user_id}','Payment\WebPaymentController
 Route::group(['prefix'=>'esewa/token'],function()
 {
     Route::get('generate/{user_id}','Payment\WebPaymentController@esewaToken');
-    Route::get('inquery/{request_id}','Payment\WebPaymentController@esewaInquery')->middleware('esewa.verify');
+    Route::group(['middleware'=>'esewa.verify'],function(){
+        Route::get('inquery/{request_id}','Payment\WebPaymentController@esewaInquery');
+    });
 });
