1d0728bd8fb6f1231e31b98d70c2c1255c95cf63
safi khan <safifalak@gmail.com>
2020-01-10 12:05:27 +0545

    update

--- app/Http/Controllers/Payment/WebPaymentController.php
+++ app/Http/Controllers/Payment/WebPaymentController.php
@@ -625,6 +625,7 @@ class WebPaymentController extends Controller
         elseif(isset($responseOb->error_key) && $responseOb->error_key === 'already_verified')
         {
             return true;
+            //throw new \Exception($responseOb->error_key, 1);
         }
         else
         {
